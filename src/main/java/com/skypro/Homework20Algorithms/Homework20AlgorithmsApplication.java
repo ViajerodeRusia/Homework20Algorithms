f946f755e809e868e975e0ac814ade15732a68ae
Denis Siliukov <abhem21rus@mail.ru>
2024-02-22 08:13:11 +0300

    Project Initialization

---
+++ src/main/java/com/skypro/Homework20Algorithms/Homework20AlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.Homework20Algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20AlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework20AlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/Homework20Algorithms/Homework20AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.Homework20Algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework20AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
